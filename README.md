1556cf76b7c3c7ea097e323bb494ed5a6de28040
dungatoro <dungatoro@arch.localdomain>
2023-11-28 20:50:01 +0000

    nice regex

--- README.md
+++ README.md
@@ -2,18 +2,16 @@
 A tool for producing source code from markdown code blocks.
 
 Following the language with a # marks that block as an example or snippet. This code will not be included in the compiled file - this is useful for breaking down code into code bit by bit in documentation.
-~~~
-```rust #
-fn f(x: i32) -> i32 {
-    ...
-}
+```python #
+def fib(x):
+    if x < 2: 
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
 ```
-~~~
 Following # with some sort of identifier allows that snippet to be compiled in isolation and ran for testing small snippets.
-~~~
 ```haskell #factorial
 fact 0 = 1
 fact n = n * fact (n-1)
 ```
-~~~
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::fs::File;
+use std::io::{self, Read};
+use regex::Regex;
+
+#[derive(Debug)]
+struct CodeBlock {
+    lang: String,
+    id: String,
+    text: String,
+}
+
+fn extract_code_blocks(path: &str) -> Result<Vec<CodeBlock>, io::Error> {
+    // Read the entire contents of the file
+    let mut file = File::open(path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+
+    // Regular expression for matching code blocks
+    let re = Regex::new(r"```([^\s]+)\s+([^\s]+)\s+([\s\S]*?)```").expect("Invalid regular expression");
+
+    // Extract code blocks using the regular expression
+    let mut code_blocks = Vec::new();
+    for capture in re.captures_iter(&content) {
+        if let (Some(lang), Some(ident), Some(body)) =
+            (capture.get(1), capture.get(2), capture.get(3))
+        {
+            let code_block = CodeBlock {
+                lang: lang.as_str().to_string(),
+                id: ident.as_str().to_string(),
+                text: body.as_str().to_string(),
+            };
+            code_blocks.push(code_block);
+        }
+    }
+    Ok(code_blocks)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "README.md";
+
+    match extract_code_blocks(file_path) {
+        Ok(blocks) => {
+            for block in blocks {
+                println!("{:?}", block);
+            }
+        }
+        Err(err) => eprintln!("Error: {}", err),
+    }
 }
